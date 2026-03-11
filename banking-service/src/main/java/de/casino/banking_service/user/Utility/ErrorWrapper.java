93ad5cfc4b24b93deb6239d5262895e070650021
Duc <Minh.Ngo@Student.HTW-Berlin.de>
2026-03-11 17:25:23 +0100

    Utility added - ErrorWrapper/-Result, Result

---
+++ banking-service/src/main/java/de/casino/banking_service/user/Utility/ErrorWrapper.java
@@ -0,0 +1,46 @@
+package de.casino.banking_service.user.Utility;
+
+import org.springframework.http.HttpStatus;
+
+
+public enum ErrorWrapper {
+    UNEXPECTED_INTERNAL_ERROR_OCCURED(
+            "An unexpected internal error occurred.",
+            HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND(
+            "A requested user could not be found.",
+            HttpStatus.NOT_FOUND),
+    USER_MODEL_INVALID_NAME_Blank_OR_NULL(
+            "The first name and last name of a user must be non-empty.",
+            HttpStatus.BAD_REQUEST),
+    USER_MODEL_INVALID_NAME_LENGTH(
+            "The first name and last name of a user must not contain more than 12 characters.",
+            HttpStatus.BAD_REQUEST),
+    USER_MODEL_INVALID_AMOUNT(
+            "The amount must be greater than 0.",
+            HttpStatus.BAD_REQUEST),
+        USER_MODEL_INSUFFICIENT_BALANCE(
+            "The user does not have sufficient balance for this operation.",
+            HttpStatus.BAD_REQUEST);
+
+
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+
+    ErrorWrapper(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+
+    public String getMessage() {
+        return message;
+    }
+
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
